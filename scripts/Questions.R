fdc16e0962ce08aaee20954b7b86e8a820600fe7
ajj762 <andreajennemann@gmail.com>
2021-09-01 10:11:18 -0500

    added questions script

---
+++ scripts/Questions.R
@@ -0,0 +1,5 @@
+# Do dispatch locations occur or accumulate in an particular areas?
+# Are there more dispatches at any certain time of day? Fewer dispatches?
+# What does "911 checks" mean?
+# Does a reason for dispatch (ExtNatureDisplayName) occur more than others? Which one occurs the least?
+# Why do only some dispatches have a Report number?
